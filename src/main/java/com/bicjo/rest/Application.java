83aab8d60015aadc7328ce1f991bcba3e2d7cf23
joven <joven@probook>
2016-09-28 00:01:28 +0800

    $

--- src/main/java/com/bicjo/rest/Application.java
+++ src/main/java/com/bicjo/rest/Application.java
@@ -3,8 +3,10 @@ package com.bicjo.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // same as @Configuration @EnableAutoConfiguration
-						// @ComponentScan
+/**
+ * same as @Configuration @EnableAutoConfiguration @ComponentScan
+ */
+@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
